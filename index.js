6ed4c99751bfdd85142f9e39c02de80c3a7ae893
Srihari-1501 <142719294+Srihari-1501@users.noreply.github.com>
2024-02-18 22:58:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+
+$(".heading").animate({
+  width: [ "toggle", "swing" ],
+  height: [ "toggle", "swing" ],
+}, 0, "linear");
+
+$(".heading").animate({
+  width: [ "toggle", "swing" ],
+  height: [ "toggle", "swing" ],
+}, 4000, "linear");
+
+
+$("#about").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 0 , "linear");
+
+$("#about").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 3000, "linear");
+
+$("#collection").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 0, "linear");
+
+$("#collection").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 3000, "linear");
+
+$("#creator").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 0, "linear");
+
+$("#creator").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 3000, "linear");
+
+$("#roadMap").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 0, "linear");
+
+$("#roadMap").animate({
+    width: [ "toggle", "swing" ],
+    height: [ "toggle", "swing" ],
+  }, 3000, "linear");
